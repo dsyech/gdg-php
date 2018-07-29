3912b6dad73470bff88d3a4ca538a289c23e27d2
dsyech <dsyech@gmail.com>
2018-07-29 13:10:15 +0700

    first commit

---
+++ helloworld.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+	<title>PHP on GAE</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Hello GAE From PHP</h1>
+		<p>
+			<?php
+				echo "PHP is !GOOD"
+			?>
+			
+		</p>
+	</div>
+</body>
+</html>
